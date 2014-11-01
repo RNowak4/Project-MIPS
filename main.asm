84f671de2f31364ade1823e29d506dc3ac5cc85d
Radek <Radek095@gmail.com>
2014-11-01 02:06:49 +0100

    Zaczynamy ogarniac

--- main.asm
+++ main.asm
@@ -9,108 +9,58 @@
 # $t7 - przechowywanie deskryptora pliku do zapisu
 #
 # Uwaga - program nie jest idioto-odporny.
+#
+# Argumenty wywolania f-cji przekazujemy w $t5 i $6
+# wynik natomiast zwracany jest w $t7
+#
+
 	
 	.data
 msg1:	.asciiz "Wpisz nazwe pliku, ktory chcesz przeksztalcic\n"
 msg2:	.asciiz "Wpisz nazwe pliku wyjsciowego\n"
-msg3:	.asciiz "Podaj parametry przeksztalcenie: a,b,c,d (wpisz liczbe -> enter - pojedynczo)\n"
-msg4:	.asciiz "Podaj kolejny parametr\n"
 finame:	.space 80 #miejsce na pobrana nazwe pliku
 foname:	.space 80
-cin:	.space 80
-data:	.space 16 #miejsce na parametry
-datast:	.space 4 #miejsce na stan danych pobranyh( 0 - liczba, 1 - sinx, 2 - siny, 3 - cosx, 4 - cos y )
-		 # Jesli 1 | 2, to wtedy nie pobieramy nic z "data", a liczymy po prostu.
+# Parametry przeksztalcenia: a,b,c,d. Przecinek umiejscowiony jest po srodku 
+# word'a, gdyz nie jest wymagana jakas wielka precyzja, a tak jest dosc wygodnie.
+A:	.word 0x0002c000
+B:	.word 0x00018000
+C:	.word 0x00000000
+D:	.word 0x00000000
 
-	.text
+	.text	
 main:
-	 li $v0, 4
-	 la $a0, msg1
-	 syscall
-	 
-	 li $v0, 8
-	 la $a0, finame
-	 li $a0, 80
-	 syscall
-	 
-	 li $v0, 4
-	 la $a0, msg2
-	 syscall
-	 
-	 li $v0, 8
-	 la $a0, foname
-	 li $a0, 80
-	 syscall
-	 
-	 li $v0, 4
-	 la $a0, msg3
-	 syscall
-	 
-	 #t1 - licznik petli
-	 li $t1, 4
-	 la $t3, data
-	 la $t4, datast
-	 jal parsuj
+	lw $t5, A
+	lw $t6, B
 	
+	jal mnoz
 	
+	li $v0, 10
+	syscall
 	
-	
-parsuj:
-	beqz $t1, powrot
 	li $v0, 4
-	la $a0, msg4
+	la $a0, msg1
 	syscall
-	
+	 
 	li $v0, 8
-	la $a0, cin
-	li $a1, 80
+	la $a0, finame
+	li $a0, 80
 	syscall
-	
-	#sprawdzamy, co to jest
-	la $t2, cin
-	lb $t0, ($t2)
-	ble $t0, '9', laduj_liczbe
-	beq $t0, 's', laduj_sin
-	beq $t0, 'c', laduj_cos
-	# w innym wypadku bedzie blad, ale zakladamy, ze uzytkownik podaje poprawne dane
-	
-laduj_liczbe:
-	li $t0, 0
-	sb $t0, ($t4)
-	
-	lw $t0, ($t2)
-	sw $t0, ($t3)
-	b dalej_parsuj
-	
-laduj_sin:
-	add $t2, $t2, 3 #sprawdzamy 3-ci znak - czy to x czy y
-	lb $t0, ($t2)
-	beq $t2, 'y', siny
-	li $t0, 1
-	sb $t0, ($t4)
-	b dalej_parsuj
-siny:	
-	li $t0, 2
-	sb $t0, ($t4)
-	b dalej_parsuj
-	
-laduj_cos:
-	add $t2, $t2, 3 #sprawdzamy 3-ci znak - czy to x czy y
-	lb $t0, ($t2)
-	beq $t2, 'y', siny
-	li $t0, 3
-	sb $t0, ($t4)
-	b dalej_parsuj
-cosy:	
-	li $t0, 4
-	sb $t0, ($t4)
-	b dalej_parsuj
-	
-dalej_parsuj:
-	add $t3, $t3, 4
-	add $t4, $t4, 1
-	sub $t1, $t1, 1
-	b parsuj
-	
-powrot:
+	 
+	li $v0, 4
+	la $a0, msg2
+	syscall
+	 
+	li $v0, 8
+	la $a0, foname
+	li $a0, 80
+	syscall
+	 
+# F-cja, ktora mnozy 2 wordy
+mnoz:
+	mult $t5, $t6
+	mfhi $t5
+	mflo $t6
+	rol $t5, $t5, 16
+	ror $t6, $t6, 16
+	or $t7, $t5, $t6
 	jr $ra
