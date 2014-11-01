dac389593f5f074a6b461cc60857be5ed62f924f
Radek <Radek095@gmail.com>
2014-11-01 03:02:49 +0100

    0.01

--- main.asm
+++ main.asm
@@ -5,8 +5,8 @@
 # $t2
 # 
 #
-# $t6 - przechowywanie deskryptora pliku czytanego
-# $t7 - przechowywanie deskryptora pliku do zapisu
+# $t6 
+# $t7 
 #
 # Uwaga - program nie jest idioto-odporny.
 #
@@ -20,6 +20,10 @@ msg1:	.asciiz "Wpisz nazwe pliku, ktory chcesz przeksztalcic\n"
 msg2:	.asciiz "Wpisz nazwe pliku wyjsciowego\n"
 finame:	.space 80 #miejsce na pobrana nazwe pliku
 foname:	.space 80
+readb:	.space 256
+writeb:	.space 256
+read:	.space 1048576
+write:	.space 1048576
 # Parametry przeksztalcenia: a,b,c,d. Przecinek umiejscowiony jest po srodku 
 # word'a, gdyz nie jest wymagana jakas wielka precyzja, a tak jest dosc wygodnie.
 A:	.word 0x0002c000
@@ -34,7 +38,7 @@ main:
 	
 	jal mnoz
 	
-	li $v0, 10
+	li $v0, 10 # szybko konczmy program.
 	syscall
 	
 	li $v0, 4
@@ -54,8 +58,11 @@ main:
 	la $a0, foname
 	li $a0, 80
 	syscall
+	
+	li $v0, 10
+	syscall
 	 
-# F-cja, ktora mnozy 2 wordy
+# F-cja, ktora mnozy 2 wordy. Argumenty sa niszczone!
 mnoz:
 	mult $t5, $t6
 	mfhi $t5
@@ -64,3 +71,18 @@ mnoz:
 	ror $t6, $t6, 16
 	or $t7, $t5, $t6
 	jr $ra
+
+# F-cja, ktora wczytuje. Argument t5 - adres stringu nazwy pliku, t6 - adres pamieci do zapisu
+wczytaj:
+	li $v0, 13
+	move $a0, $t5
+	li $a1, 0
+	li $a2, 0
+	
+	move $t5, $v0 # Deskryptor pliku jest w $t5
+	
+petla:
+	# zrobic tak, ze wczytujemy paczke bajtow i od razu obliczamy wspolrzedne, gdzie co ma pojsc.
+	# wtedy nie trzeba wczytywac do pamieci i jest gitez. Nalezy tez pewnie wyzerowac przestrzen
+	# zarezerwowana dla pliku wyjsciowego. Tutaj nie da sie od tak tego zapisac, bo nalezy miec
+	# dotesp do kazdego pixela w dowolnym momenecie.
